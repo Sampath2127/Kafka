4cab9412f9c880bba7e2cdaf4c2d5b547ecf92ec
sampath reddy <sampathreddy2127@gmail.com>
2020-07-19 22:34:01 -0500

    Schema evolution backward and forward compatibility for schemas

--- KafkaAvroData/src/main/java/com/lovetocode/avro/schemaevolution/SchemaEvolutionCustomer.java
+++ KafkaAvroData/src/main/java/com/lovetocode/avro/schemaevolution/SchemaEvolutionCustomer.java
@@ -25,15 +25,15 @@ public class SchemaEvolutionCustomer {
         customerBuilder.setHeight (178f);
         customerBuilder.setWeight (75.00f);
         customerBuilder.setAutomatedEmail (false);
-        CustomerV1 customer = customerBuilder.build ();
+        CustomerV1 customerV1 = customerBuilder.build ();
 
-        System.out.println ("Customer v1 "+customer.toString ());
+        System.out.println ("Customer v1 "+customerV1.toString ());
 
         // write the data of customer object to a file
         final DatumWriter<CustomerV1> datumWriter = new SpecificDatumWriter<> (CustomerV1.class);
         try( DataFileWriter<CustomerV1> dataFileWriter = new DataFileWriter<> (datumWriter) ){
             dataFileWriter.create (CustomerV1.SCHEMA$, new File ("customer_v1.avro"));
-            dataFileWriter.append (customer);
+            dataFileWriter.append (customerV1);
         } catch ( IOException e ) {
             System.out.println ("Error while writing data to file..!");
             e.printStackTrace ();
@@ -47,6 +47,45 @@ public class SchemaEvolutionCustomer {
             while ( dataFileReader.hasNext () ){
                 CustomerV2 readCustomer = dataFileReader.next ();
                 System.out.println ("Customer v2 "+readCustomer.toString ());
+//                System.out.println (customer.getFirstName ());
+//                System.out.println (customer.getHeight ());
+            }
+        } catch ( IOException e ) {
+            System.out.println ("Unable to read data from Schema object..!");
+            e.printStackTrace ();
+        }
+
+        // create a specific record object from schema v1
+        CustomerV2.Builder customerBuilderv2 = CustomerV2.newBuilder ();
+        customerBuilderv2.setAge (25);
+        customerBuilderv2.setFirstName ("Sam");
+        customerBuilderv2.setLastName ("Kom");
+        customerBuilderv2.setHeight (178f);
+        customerBuilderv2.setWeight (75.00f);
+        customerBuilderv2.setPhoneNo ("123-456-7890");
+        customerBuilderv2.setEmail ("customerv2@gmail.com");
+        CustomerV2 customerV2 = customerBuilderv2.build ();
+
+        System.out.println ("Customer v2 : "+customerV2.toString ());
+
+        // write the data of customer object to a file
+        final DatumWriter<CustomerV2> datumWriterv2 = new SpecificDatumWriter<> (CustomerV2.class);
+        try( DataFileWriter<CustomerV2> dataFileWriter = new DataFileWriter<> (datumWriterv2) ){
+            dataFileWriter.create (CustomerV2.SCHEMA$, new File ("customer_v2.avro"));
+            dataFileWriter.append (customerV2);
+        } catch ( IOException e ) {
+            System.out.println ("Error while writing data to file..!");
+            e.printStackTrace ();
+        }
+
+
+        //read data from avro schema file using new schema v1
+        File filev2 = new File ("customer_v2.avro");
+        final DatumReader<CustomerV1> datumReaderV2 = new SpecificDatumReader<> (CustomerV1.class);
+        try( DataFileReader<CustomerV1> dataFileReader = new DataFileReader<> (filev2, datumReaderV2) ){
+            while ( dataFileReader.hasNext () ){
+                CustomerV1 readCustomer = dataFileReader.next ();
+                System.out.println ("Customer v1 : "+readCustomer.toString ());
 //                System.out.println (customer.getFirstName ());
 //                System.out.println (customer.getHeight ());
             }
